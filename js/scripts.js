2d33f7261c52eebd53b142cf8006f63a2ee49153
Jason Campbell <43692594+jcampbell18@users.noreply.github.com>
2019-03-18 14:56:29 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,27 @@
+console.log('OK');
+
+var screenWidth = $(window).width();
+var videoWidth = 640;
+var videoHeight = 360;
+var aspectRatio = videoWidth/videoHeight;
+
+if (screenWidth < 640) {
+    videoWidth = screenWidth * .99;
+    videoHeight = videoWidth / aspectRatio;
+}
+
+console.log('Screen width: ' + screenWidth);
+console.log('Video wxh: ' + videoWidth + ' x ' + videoHeight);
+
+/* Media Element Player Plugin
+* Reference: https://github.com/mediaelement/mediaelement/blob/master/docs/usage.md
+*/
+$('#mediaplayer').mediaelementplayer({
+
+    defaultVideoWidth: videoWidth,
+    defaultVideoHeight: videoHeight
+   
+});
+
 // Output Current Year
 $('.currentYear').text( (new Date).getFullYear() );
