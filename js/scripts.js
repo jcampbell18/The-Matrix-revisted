570b477e0f326f7efefe7037a38763ce5a327d30
Jason C (Desktop) <43692594+jcampbell18@users.noreply.github.com>
2019-03-18 20:28:08 -0700

    Update scripts.js
    just put TODO notes in there

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
+/* Navigation */
+/* TODO:    on hover, activate matrix gif
+ *          on mouse press, deactivate matrix gif
+
+$('header').
+$('header').css();
+*/
+
+/* Screenshots
+ * TODO:   lightbox
+ *         random images?
+ */ 
+
+/* Polygon grid 
+ * TODO:    on hover, display actor's name
+ *          lightbox?
+ *          link to imdb?
+ */
+
 /*
  * determining screen size, and adjusting video (responsive)
  */
-var screenWidth = $(window).width();
-var videoWidth = 640;
-var videoHeight = 360;
+var screenWidth = $(window).width();        
+var videoWidth = 640;                       // max width
+var videoHeight = 360;                      // max height
 var aspectRatio = videoWidth/videoHeight;
 
 if (screenWidth < 640) {
@@ -11,6 +30,7 @@ if (screenWidth < 640) {
     videoHeight = videoWidth / aspectRatio;
 }
 
+/* Diagnostics */
 console.log('Screen width: ' + screenWidth);
 console.log('Video (w x h): ' + videoWidth + ' x ' + videoHeight);
 
